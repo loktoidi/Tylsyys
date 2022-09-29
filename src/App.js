149fc78fe940072e49ec1722fd7eedd55cc5fb2b
loktoidi <loktoidi@luukku.com>
2022-09-29 15:05:57 +0300

    valmis tekele

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App(){
+  function loadActivity(){
+      setIsLoading(true);
+      fetch('https://www.boredapi.com/api/activity')
+  .then((response) => response.json())
+  .then((data) => setActivity(data.activity));
+      setIsLoading(false)
+
+  }
+
+  const [activity, setActivity] = useState([]);
+const [isLoading, setIsLoading] = useState(true);
+useEffect(() => {
+    loadActivity();
+},[]);
+
+if(isLoading) {
+    return <p>Lataa...</p>
+}
+
+return (
+  <div>
+    <h3>Tylsyys tyrmääjä.</h3>
+      <h1>{activity}</h1>
+      <button onClick={loadActivity}>Anna toinen</button>
+  </div>
+);
 }
 
 export default App;
+
+
+
